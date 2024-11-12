f4d2f955e3868b0ea4b786f15ed8bed019a953a1
Ashish Bhandari <officialashish555@gmail.com>
2024-11-12 21:32:29 +0530

    add basic homepage layout

---
+++ components/FeedCard/index.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import React from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BiMessageRounded, BiUpload } from "react-icons/bi";
+import { FaRetweet } from "react-icons/fa";
+
+const FeedCard: React.FC = () => {
+  return (
+    <div className="border b-r-0 border-b-0 border-l-0 border-gray-600 p-5 hover:bg-slate-900 transition-all cursor-pointer">
+      <div className="grid grid-cols-12 gap-3">
+        <div className="col-span-1">
+          <Image
+            className="rounded-full"
+            src="https://avatars.githubusercontent.com/u/130917004?v=4"
+            alt="user-image"
+            height={50}
+            width={50}
+          />
+        </div>
+        <div className="col-span-11">
+          <h5>Ashish Bhandari</h5>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+            molestiae.
+          </p>
+          <div className="flex justify-between mt-5 text-xl items-center w-[90%]">
+            <div>
+              <BiMessageRounded />
+            </div>
+            <div>
+              <FaRetweet />
+            </div>
+            <div>
+              <AiOutlineHeart />
+            </div>
+            <div>
+              <BiUpload />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedCard;
---
+++ pages/index.tsx
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
+import { BiHash, BiHomeCircle, BiMoney, BiUser } from "react-icons/bi";
+import FeedCard from "@/components/FeedCard";
+import { SlOptions } from "react-icons/sl";
+
+interface TwitterSidebarButton {
+  title: string;
+  icon: React.ReactNode;
+}
+
+const sidebarMenuItems: TwitterSidebarButton[] = [
+  {
+    title: "Home",
+    icon: <BiHomeCircle />,
+  },
+  {
+    title: "Explore",
+    icon: <BiHash />,
+  },
+  {
+    title: "Notifications",
+    icon: <BsBell />,
+  },
+  {
+    title: "Messages",
+    icon: <BsEnvelope />,
+  },
+  {
+    title: "Bookmarks",
+    icon: <BsBookmark />,
+  },
+  {
+    title: "Twitter Blue",
+    icon: <BiMoney />,
+  },
+  {
+    title: "Profile",
+    icon: <BiUser />,
+  },
+  {
+    title: "More Options",
+    icon: <SlOptions />,
+  },
+];
+export default function Home() {
+  return (
+    <div>
+      <div className="grid grid-cols-12 h-screen w-screen px-56">
+        <div className="col-span-3 pt-1 px-4">
+          <div className="text-2xl h-fit w-fit hover:bg-gray-800 rounded-full p-4 cursor-pointer transition-all">
+            <BsTwitter />
+          </div>
+          <div className="mt-2 text-xl pr-3">
+            <ul>
+              {sidebarMenuItems.map((item) => (
+                <li
+                  className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-3 py-2 w-fit cursor-pointer mt-2"
+                  key={item.title}
+                >
+                  <span className="text-3xl">{item.icon}</span>
+                  <span className="text-lg">{item.title}</span>
+                </li>
+              ))}
+            </ul>
+            <div className="mt-5 px-3">
+              <button className="bg-[#1d9bf0] px-4 py-2 rounded-full text-lg font-semibold  w-full">
+                Tweet
+              </button>
+            </div>
+          </div>
+        </div>
+        <div
+          className="col-span-5 border-r-[1px] 
+        border-l-[1px] h-screen overflow-scroll border-gray-600"
+        >
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+        </div>
+        <div className="col-span-3"></div>
+      </div>
+    </div>
+  );
+}
